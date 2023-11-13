3058cad8bdd4088d8c8cf4f65028ed1ac71e7cc7
fzh <fan-zh22@mails.tsinghua.edu.cn>
2023-11-13 21:50:04 +0800

    create repository

---
+++ main.py
@@ -0,0 +1,16 @@
+import torch
+import gym
+
+def make_env(gym_id, seed, idx, capture_video, run_name):
+    def thunk():
+        env = gym.make(gym_id)
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        if capture_video:
+            if idx == 0:
+                env = gym.wrappers.RecordVideo(env, f"videos/{run_name}")
+        env.seed(seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+        return env
+
+    return thunk
